492ca2bfc6eace3cfd487a7a2bcfea2b80091abb
heinz2401 <162138350+heinz2401@users.noreply.github.com>
2024-10-28 11:30:28 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criarCartao(
+    'historia',
+    'Em que ano foi descoberto o Brasil?',
+    '22 de abril de 1500'
+)
+criarCartao(
+    'historia',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Manuel Deodoro da Fonseca'
+)
+criarCartao(
+    'historia',
+    'Quem descobrio o Brasil?',
+    'Pedro Álvares Cabral'
+criarCartao(
+    'matematica',
+    '1+1?',
+    '=2'
+)
+criarCartao(
+    'marematica',
+    '2+2?'
+    '=4'
+)
